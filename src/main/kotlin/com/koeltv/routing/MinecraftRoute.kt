e26cbfb5442c4db8046006772862fe4ab7d2d28f
koeltv <Valentin.koeltgen@utt.fr>
2023-04-26 23:46:13 +0200

    Added forge version handling

--- src/main/kotlin/com/koeltv/routing/MinecraftRoute.kt
+++ src/main/kotlin/com/koeltv/routing/MinecraftRoute.kt
@@ -8,7 +8,10 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.nio.file.InvalidPathException
 import java.nio.file.Paths
+
+private const val LIBRARIES_PATH = "server/libraries/net/minecraftforge/forge"
 
 var serverProcess: Process? = null
 
@@ -42,16 +45,23 @@ fun Route.configureMinecraftRoutes() {
 
 private suspend fun startServer(): Process {
     return withContext(Dispatchers.IO) {
+        val currentDirectory = Paths.get("")
 
-        val serverDirectory = Paths.get("").resolve("server").toFile()
+        val forgeVersion = currentDirectory
+            .resolve(LIBRARIES_PATH)
+            .toFile()
+            .list()
+            ?.firstOrNull()
+            ?: throw InvalidPathException("$this/$LIBRARIES_PATH", "Incorrect server files structure")
 
-        // TODO Handle server version
+        val serverDirectory = currentDirectory.resolve("server").toFile()
+
         ProcessBuilder()
             .directory(serverDirectory)
             .command(
                 "java",
                 "@user_jvm_args.txt",
-                "@libraries/net/minecraftforge/forge/1.18.2-40.2.2/${
+                "@libraries/net/minecraftforge/forge/$forgeVersion/${
                     if (currentOs.contains("win")) {
                         "win"
                     } else {
